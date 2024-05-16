c14e3a82f6d902cafec6f1a78cf73cce86b2c38e
Markzhang8617 <mzhang8617@conestogac.on.ca>
2024-05-16 16:24:22 -0400

    Calculator commit

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,91 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by user246846 on 5/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var LabelCalculator: UILabel!
+    
+    @IBOutlet weak var LableLine1: UITextField!
+    
+    @IBOutlet weak var LabelLine2: UITextField!
+    
+    @IBOutlet weak var Line3: UITextField!
+    
+    @IBAction func AC(_ sender: Any) {
+    }
+    
+    @IBAction func PlusMinus(_ sender: Any) {
+    }
+    
+    @IBAction func Square(_ sender: Any) {
+    }
+    
+    @IBAction func Divide(_ sender: Any) {
+    }
+    
+    @IBAction func Multiple(_ sender: Any) {
+    }
+    
+    @IBAction func Plus(_ sender: Any) {
+    }
+    
+    @IBAction func Minus(_ sender: Any) {
+    }
+    
+    @IBAction func Dot(_ sender: Any) {
+    }
+    
+    @IBAction func Equals(_ sender: Any) {
+    }
+    
+    @IBAction func One(_ sender: Any) {
+    }
+    
+    @IBAction func Two(_ sender: Any) {
+    }
+    
+    @IBAction func Three(_ sender: Any) {
+    }
+    
+    @IBAction func Four(_ sender: Any) {
+    }
+    
+    @IBAction func Five(_ sender: Any) {
+    }
+    
+    @IBAction func Six(_ sender: Any) {
+    }
+    
+    @IBAction func Seven(_ sender: Any) {
+    }
+    
+    @IBAction func Eight(_ sender: Any) {
+    }
+    
+    @IBAction func Nine(_ sender: Any) {
+    }
+    
+    @IBAction func Zero(_ sender: Any) {
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        
+        
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
